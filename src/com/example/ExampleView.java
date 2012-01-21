8c13759d3d09d6545e52427098cc6b120b171359
Jason Ostrander <jason.ostrander@gmail.com>
2012-01-20 21:02:44 -0800

    Save work on making a triangle.

--- src/com/example/ExampleView.java
+++ src/com/example/ExampleView.java
@@ -2,6 +2,7 @@ package com.example;
 
 import android.content.Context;
 import android.renderscript.Float4;
+import android.renderscript.Mesh;
 import android.renderscript.RSSurfaceView;
 import android.renderscript.RenderScriptGL;
 import android.renderscript.RenderScriptGL.SurfaceConfig;
@@ -16,6 +17,7 @@ public class ExampleView extends RSSurfaceView {
         final RenderScriptGL.SurfaceConfig sc = new SurfaceConfig();
         mRS = createRenderScriptGL(sc);
         mScript = new ScriptC_example(mRS, getResources(), R.raw.example);
+        buildTriangle();
         mRS.bindRootScript(mScript);
     }
 
@@ -26,11 +28,22 @@ public class ExampleView extends RSSurfaceView {
         case MotionEvent.ACTION_MOVE:
             float x = event.getX()/getWidth();
             float y = event.getY()/getHeight();
-
             Float4 color = new Float4(x, y, 0.5f, 1.0f);
             mScript.set_bgColor(color);
+//            mScript.set_gRotation(event.getX());
             return true;
         }
         return super.onTouchEvent(event);
+    }
+
+    public void buildTriangle() {
+        Mesh.TriangleMeshBuilder triangles = new Mesh.TriangleMeshBuilder(mRS,
+                2, Mesh.TriangleMeshBuilder.COLOR);
+        triangles.addVertex(0.f, -75.0f);
+        triangles.addVertex(-150.0f, 75.0f);
+        triangles.addVertex(150.0f, 75.0f);
+        triangles.addTriangle(0, 1, 2);
+        Mesh smP = triangles.create(true);
+        mScript.set_mesh(smP);
     }
 }
--- src/com/example/example.rs
+++ src/com/example/example.rs
@@ -5,22 +5,36 @@
  
  // Background color is a 4 part float
 float4 bgColor;
- 
+
+// Triangle mesh
+rs_mesh mesh;
+
+// Rotation float
+float gRotation;
+
 void init() {
     // Initialize background color to black
     bgColor = (float4) { 0.0f, 0.0f, 0.0f, 1.0f };
+    gRotation = 0.0f;
 }
- 
+
 int root() {
     // Set background color
     rsgClearColor(bgColor.x, bgColor.y, bgColor.z, bgColor.w);
     
-    // Set font color
-    rsgFontColor(1.0f, 1.0f, 1.0f, 1.0f);
+    // Load matrix for translate and rotate
+    rs_matrix4x4 matrix;
+    rsMatrixLoadIdentity(&matrix);
+    rsMatrixTranslate(&matrix, 300.0f, 300.0f, 0.0f);
+    rsMatrixRotate(&matrix, gRotation, 0.0f, 0.0f, 1.0f);
+    rsgProgramVertexLoadModelMatrix(&matrix);
     
-    // Draw text at position 50, 50
-    rsgDrawText("Hello World!", 50.0f, 50.0f);
+    // draw the triangle mesh
+    rsgDrawMesh(mesh);
     
+    // Animate rotation
+    gRotation += 1.0f;
+
     // Run every 20 milliseconds
     return 20;
 }
